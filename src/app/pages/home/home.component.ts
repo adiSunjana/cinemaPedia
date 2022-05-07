e2abad94b393868cc7294177f2b86d211047c80a
adiSunjana <adisunjana443@gmail.com>
2022-05-07 08:21:38 +0700

    home service & layouts service

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { HomeService } from 'src/app/services/home.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit , OnDestroy{
 
-  constructor() { }
+  private readonly unsubs : Subject<void> = new Subject<void>() 
+  constructor( private home : HomeService ) { }
 
   ngOnInit(): void {
+    this.getMoviePopular()
+  }
+
+  getMoviePopular()
+  {
+    this.home.popularMovie()
+              .pipe(
+                takeUntil(this.unsubs)
+              )
+              .subscribe(
+                ( res : any ) => {
+                  console.log(res)
+                },
+                err => {
+                  console.log(err)
+                }
+              )
+  }
+
+
+  ngOnDestroy(): void {
+    this.unsubs.next()
+    this.unsubs.complete()
+    this.unsubs.unsubscribe()
   }
 
 }
--- src/app/services/home.service.ts
+++ src/app/services/home.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiService } from './api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HomeService {
 
-  constructor() { }
+  constructor( private api : ApiService ) { }
+
+  popularMovie() : Observable<any>
+  {
+    return this.api.getData('MostPopularMovies') as Observable<any>
+  }
+  
 }
