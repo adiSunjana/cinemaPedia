caee1fea8a1c5ab283fdf2cd73c8474593f7aab5
adiSunjana <adisunjana443@gmail.com>
2022-05-22 10:13:40 +0700

    add header , & update

--- src/app/layouts/layouts.module.ts
+++ src/app/layouts/layouts.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
   declarations: [
-    FooterComponent
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule
   ],
   exports : [
-    FooterComponent
+    FooterComponent,
+    HeaderComponent
   ]
 })
 export class LayoutsModule { }
