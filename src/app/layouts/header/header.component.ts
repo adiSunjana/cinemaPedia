35024cbd5d0a9f3a91953e30b200710459efd20e
adiSunjana <adisunjana443@gmail.com>
2022-05-22 12:56:46 +0700

    add activated route & ckass activate

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -8,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   public arrOpen : boolean = false;
+  public menus : Array<any> = [
+    { name : 'Home' , label : 'home' },
+    { name : 'News' , label : 'news' },
+    { name : 'About' , label : 'about' },
+    { name : 'FAQ' , label : 'faq' },
+  ]
   constructor() { }
 
   ngOnInit(): void {
